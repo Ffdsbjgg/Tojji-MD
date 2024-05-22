6a1508e5372acadb47d2beca03284235b225edc8
rfjfdejdftk <164313907+rfjfdejdftk@users.noreply.github.com>
2024-05-22 14:13:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
 global.imagen4 = fs.readFileSync('./menu2.mp4');
 global.imagen5 = fs.readFileSync('./src/+18.jpg');
-global.imagen6 = fs.readFileSync('./Menu3.png');
+global.imagen6 = fs.readFileSync('./menu3.mp4');
 
 global.mods = [];
 
