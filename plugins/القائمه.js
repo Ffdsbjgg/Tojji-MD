510b6c67694f01828626111706606fdb7008c052
rfjfdejdftk <164313907+rfjfdejdftk@users.noreply.github.com>
2024-05-23 16:54:33 +0300

    Update القائمه.js

--- plugins/القائمه.js
+++ plugins/القائمه.js
@@ -79,7 +79,7 @@ global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remot
 > *_لا تتنسي أن تنظر الا قسم البنك "#13"_*
 > *_⭔ ᴋᴀɴᴇᴋɪ ʙᴏᴛ ᴍᴅ ⭓_*
 `
-const vMimg = 'https://imgur.com/a/1A6emzC'
+const vMimg = 'https://imgur.com/a/wHm8nrT'
 
     const { result, key, timeout } = await conn.sendMessage(m.chat, { video: { url: vMimg }, caption: str.trim(),  gifPlayback: true,
   gifAttribution: 0}, { quoted: fcontact })
