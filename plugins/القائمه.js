8711b2eb2fbe5aa72b20a738bec1dc81d240aaaa
rfjfdejdftk <164313907+rfjfdejdftk@users.noreply.github.com>
2024-05-23 17:17:26 +0300

    Update القائمه.js

--- plugins/القائمه.js
+++ plugins/القائمه.js
@@ -35,7 +35,7 @@ let rtotalreg = Object.values(global.db.data.users).filter(user => user.register
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(850) 
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}
+global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}
     const str = `*🛡️KANEKI-BOT-MÐ🛡️*
 ╮━━━━━━━━━━━━━╼⟢ـ
 ┃📌⎇ مرحـبـاً:  ${user}
