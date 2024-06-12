ac1fd2717424c97f235dc2c1888546e22906f7cf
rfjfdejdftk <164313907+rfjfdejdftk@users.noreply.github.com>
2024-06-12 13:21:09 +0100

    Delete plugins directory
    Signed-off-by: rfjfdejdftk <164313907+rfjfdejdftk@users.noreply.github.com>

--- plugins/tools-name1.js
+++
@@ -1,9 +0,0 @@
-let handlerMyNickname = async (m, { conn }) => {
-    let mentioned = m.quoted ? m.quoted.sender : m.sender;
-    let nicknames = global.db.data.nicknames || {};
-    let nickname = nicknames[mentioned] ||  "لست مسجلا في قاعدة بياناتي";
- let venomN = "🍡╎لـقـبـك هـو : " + nickname
-    conn.reply(m.chat, venomN, m)
-};
-handlerMyNickname.command = /^لقبي$/i;
-export default handlerMyNickname;
